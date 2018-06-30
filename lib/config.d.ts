4b721206e762d5aa1fee965eb04cd5325d6944a8
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-07-01 00:44:06 +0900

    libが入ったバージョン, package.jsonでgithubを指定する用

---
+++ lib/config.d.ts
@@ -0,0 +1,82 @@
+interface WWAConfigWithDefaults {
+    urlGateEnable: boolean;
+    resources: {
+        mapdata: string;
+        loader: string;
+        audio: {
+            dir: string;
+            js: string;
+        };
+        wwaJs: string;
+        wwaCss: string;
+        titleImg?: string;
+        cryptoJsInDevMode?: string;
+    };
+}
+interface CopyrightWithDefaults {
+    range?: {
+        firstYear: number;
+        lastYear: number | "present";
+    };
+    product: {
+        name: string;
+        href: string;
+    };
+    credit: string;
+    genre?: string;
+}
+interface Utils {
+    thisYear: number;
+    concatDirAndFile: (dir: string, file: string) => string;
+}
+interface WWAPageConfigWithDefaults {
+    page: {
+        template: string;
+        title: string;
+        isDevMode: boolean;
+        wwa: WWAConfigWithDefaults;
+        copyrights?: CopyrightWithDefaults[];
+    };
+}
+export declare type WWAPageConfigForRendering = WWAPageConfigWithDefaults & {
+    utils: Utils;
+};
+export interface WWAConfig {
+    urlgateEnable: boolean;
+    resources?: {
+        mapdata: string;
+        loader?: string;
+        audio?: {
+            dir?: string;
+            js?: string;
+        };
+        wwaJs?: string;
+        wwaCss?: string;
+        titleImg?: string;
+        cryptoJsInDevMode?: string;
+    };
+}
+export interface Copyright {
+    range?: {
+        firstYear: number;
+        lastYear?: number | "present";
+    };
+    product: {
+        name: string;
+        href: string;
+    };
+    credit: string;
+    genre?: string;
+}
+export interface WWAPageConfig {
+    page: {
+        template?: string;
+        title?: string;
+        isDevMode?: boolean;
+        wwa: WWAConfig;
+        copyrights?: Copyright[];
+    };
+}
+export declare function fillDefaultsAndUtil(wwaPageConfig: WWAPageConfig): WWAPageConfigForRendering;
+export declare function getConfigFromFile(configFilePath: string): WWAPageConfigForRendering;
+export {};
---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { WWAPageConfig } from "./config";
+export { WWAPageConfig };
+export declare function generateWWAPatgeFromConfigFile(configFilePath: string): string;
+export declare function generateWWAPageFromConfig(inputConfig: WWAPageConfig): string;
